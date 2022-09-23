d16922dc1b7907c3f2295581e62b8f951d6f0cf4
apromac <abraham.tiene@apromac.ci>
2022-09-23 00:58:57 +0000

    Mise à jour de l'implementation

--- src/main/java/com/apromac/saigneur/repository/OccuperRepository.java
+++ src/main/java/com/apromac/saigneur/repository/OccuperRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface OccuperRepository extends JpaRepository<OccuperEntity, Long> {
     public List<OccuperEntity> findByUtilisateur(UtilisateurEntity utilisateur);
 
-    @Query("SELECT o FROM Occuper o WHERE o.utilisateur.utilisateurID = :utilisateurID AND o.isOccuper = :isActif")
+    @Query("SELECT o FROM OccuperEntity o WHERE o.utilisateur.utilisateurID = :utilisateurID AND o.isOccuper = :isActif")
     public OccuperEntity findByUtilisateurAndOccuperIsTrue(Long utilisateurID, Boolean isActif);
 
     public List<OccuperEntity> findByPoste(PosteEntity poste);
