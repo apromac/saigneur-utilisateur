a7b30364d50395bcc08489892aa03ef981535f60
apromac <abraham.tiene@apromac.ci>
2022-11-05 11:52:52 +0000

    Implementation saigneur-utilisateur

--- src/main/java/com/apromac/saigneur/serviceimpl/ProfilServiceImpl.java
+++ src/main/java/com/apromac/saigneur/serviceimpl/ProfilServiceImpl.java
@@ -73,7 +73,7 @@ public class ProfilServiceImpl implements ProfilService {
      * @return profilSave
      */
     public ProfilEntity saveProfil(ProfilEntity profil) {
-        profil.getLibelleProfil().toUpperCase();
+        profil.setLibelleProfil(profil.getLibelleProfil().toUpperCase());
         ProfilEntity profilSave = profilRepository.save(profil);
         if (profilSave == null)
             throw new RuntimeException("Une erreur est survenu lors de la sauvegarde du profil.");
